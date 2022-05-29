b88f9269c3096c52a7376901f0ab98b3ee03b7a2
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-05-29 18:36:44 +0200

    Auto update

--- install.bat
+++ install.bat
@@ -5,6 +5,7 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 )
 if exist "%~dp0Raid-Bot\bot.py" (
     cd Raid-Bot
+    python auto-update.py
     python bot.py
 ) else (
     git clone https://github.com/madkarmaa/Raid-Bot.git
@@ -16,7 +17,6 @@ if exist "%~dp0Raid-Bot\bot.py" (
     echo.
     pause >nul
     cls
-    python auto-update.py
     python bot.py
 )
 :colorEcho
