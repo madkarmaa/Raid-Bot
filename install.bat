5d40d4aea94d24e8568ad772f237dc9e89407d9a
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-05-30 15:42:00 +0200

    Auto update upgrade

--- install.bat
+++ install.bat
@@ -5,7 +5,8 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 )
 if exist "%~dp0Raid-Bot\bot.py" (
     cd Raid-Bot
-    python auto-update.py
+    python -c 'import autoUpdate; autoUpdate.botCheck()'
+    python -c 'import autoUpdate; autoUpdate.autoupdateCheck()'
     python bot.py
 ) else (
     git clone https://github.com/madkarmaa/Raid-Bot.git
