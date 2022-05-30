1c90af8c1fb0c655a04dd1aa772b80ac425b543f
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-05-30 15:53:35 +0200

    Auto update upgrade failed. Reverting.

--- install.bat
+++ install.bat
@@ -5,8 +5,7 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 )
 if exist "%~dp0Raid-Bot\bot.py" (
     cd Raid-Bot
-    python -c 'import autoUpdate; autoUpdate.botCheck()'
-    python -c 'import autoUpdate; autoUpdate.autoupdateCheck()'
+    python auto-update.py
     python bot.py
 ) else (
     git clone https://github.com/madkarmaa/Raid-Bot.git
