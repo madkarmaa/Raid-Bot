fe29991a3b175a9919b0775851d4478dc48dc734
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-05-30 17:10:49 +0200

    Auto update upgraded

--- auto-update.py
+++ auto-update.py
@@ -12,22 +12,47 @@ def clearConsole(): return os.system(
     'cls' if os.name in ('nt', 'dos') else 'clear')
 
 
-link = "https://raw.githubusercontent.com/madkarmaa/Raid-Bot/main/bot.py"
-f = requests.get(link)
+linkBot = "https://raw.githubusercontent.com/madkarmaa/Raid-Bot/main/bot.py"
+linkAuto = "https://raw.githubusercontent.com/madkarmaa/Raid-Bot/main/auto-update.py"
+f = requests.get(linkBot)
+f2 = requests.get(linkAuto)
 onlineResult = f.text
+onlineResult2 = f2.text
 
 with open("bot.py", "r") as original:
     originalResult = original.read()
     original.close()
 
-if onlineResult == originalResult:
+with open("auto-update.py", "r") as original2:
+    originalResult2 = original2.read()
+    original2.close()
+
+if onlineResult == originalResult and onlineResult2 == originalResult2:
     exit()
-else:
+if onlineResult != originalResult:
     print(Fore.YELLOW +
           'An update for "bot.py" has been found. Do you want to download it?' + Fore.RESET)
     confirm = input("(Y/N): ")
     if confirm.lower() in ["yes", "y", "true"]:
-        urllib.request.urlretrieve(link, "bot.py")
+        urllib.request.urlretrieve(linkBot, "bot.py")
+        print(Fore.GREEN + "Update successfully downloaded.")
+        time.sleep(3.0)
+        clearConsole()
+        exit()
+    elif confirm.lower() in ["no", "n", "false"]:
+        clearConsole()
+        exit()
+    else:
+        print(Fore.RED + "Error. Loading current version...")
+        time.sleep(3.0)
+        clearConsole()
+        exit()
+if onlineResult2 != originalResult2:
+    print(Fore.YELLOW +
+          'An update for "auto-update.py" has been found. Do you want to download it?' + Fore.RESET)
+    confirm = input("(Y/N): ")
+    if confirm.lower() in ["yes", "y", "true"]:
+        urllib.request.urlretrieve(linkAuto, "auto-update.py")
         print(Fore.GREEN + "Update successfully downloaded.")
         time.sleep(3.0)
         clearConsole()
